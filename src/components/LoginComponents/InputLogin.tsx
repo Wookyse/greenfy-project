9fcd13d817c631237579913d5d6e36e3926f1e31
Guilherme <coelhoguilherme688@gmail.com>
2022-04-15 21:48:31 -0300

    ajustes nos campos input e svg icons

--- src/components/LoginComponents/InputLogin.tsx
+++ src/components/LoginComponents/InputLogin.tsx
@@ -1,17 +1,21 @@
 import './style.scss';
-import { GroupMail } from '../../assets/icons/Group.svg';
-import { GroupPass } from '../../assets/icons/charm_shield-keyhole.svg';
+import { ReactComponent as GroupMail } from '../../assets/icons/Group.svg';
+import { ReactComponent as GroupPass } from '../../assets/icons/charm_shield-keyhole.svg';
 export const InputLogin = () => {
   return (
     <div className="input-login">
-      <span>
-        <GroupMail />
+      <div className="input-email-field">
+        <div className="mail-ico-field">
+          <GroupMail />
+        </div>
         <input type="text" placeholder="Institutional email" />
-      </span>
-      <span>
-        <GroupPass />
+      </div>
+      <div className="input-password-field">
+        <div className="pass-ico-field">
+          <GroupPass />
+        </div>
         <input type="text" placeholder="Password" />
-      </span>
+      </div>
     </div>
   );
 };
